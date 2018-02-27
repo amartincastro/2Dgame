71d51cee4bc7ae59c8fe8d4d8798488e7147a9e1
amartincastro <castroarielm@gmail.com>
2018-02-27 15:38:48 -0800

    Add jump physics

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,15 +4,27 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 2;
+    [SerializeField]
+    private float jumpForce = 400;
 
-    // Update is called once per frame
-    private void Update()
+    private new Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical);
+        Vector3 movement = new Vector3(horizontal, 0);
 
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
