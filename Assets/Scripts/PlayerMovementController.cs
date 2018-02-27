32b0716b2e9eee28db11df9de3bafea01c21b3a3
amartincastro <castroarielm@gmail.com>
2018-02-27 12:10:03 -0800

    Add movement to character

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 2;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
